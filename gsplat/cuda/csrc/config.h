b744550b70497aa0f2c8845f8ffddcb9afff285f
junjie_wang <Junjie_DC@foxmail.com>
2025-05-14 16:03:33 +0200

    add cube test

--- gsplat/cuda/csrc/config.h
+++ gsplat/cuda/csrc/config.h
@@ -1,12 +1,12 @@
-#define BLOCK_X 1
-#define BLOCK_Y 1
-#define BLOCK_Z 1
+#define BLOCK_X 6
+#define BLOCK_Y 6
+#define BLOCK_Z 6
 // #define BLOCK_SIZE (BLOCK_X * BLOCK_Y * BLOCK_Z)
 #define N_THREADS 256
 
 #define MAX_REGISTER_CHANNELS 256
 #define MAX_POINTS_PER_THREAD 16  // 一个 block最多处理 256 * 16 = 4096 个点
-#define CHANNELS 2
+#define CHANNELS 1
 
 #define CUDA_CALL(x)                                                           \
     do {                                                                       \
---
+++ tests/fit_one_cube_interactive.py
@@ -0,0 +1,178 @@
+# 测试一个cube看xyz如何移动
+
+import torch
+import torch.nn as nn
+import numpy as np
+import trimesh
+from scipy.spatial.transform import Rotation
+from gsplat.project_gaussians import project_gaussians
+from gsplat.rasterize_sum import rasterize_gaussians_sum
+from tqdm import tqdm
+import math
+import pyvista as pv
+from PIL import Image
+import io
+
+np.random.seed(1)
+torch.manual_seed(1)
+
+def random_quat_tensor(N):
+    """
+    Defines a random quaternion tensor of shape (N, 4)
+    """
+    u = torch.rand(N, 1)
+    v = torch.rand(N, 1)
+    w = torch.rand(N, 1)
+    return torch.cat(
+        [
+            torch.sqrt(1 - u) * torch.sin(2 * math.pi * v),
+            torch.sqrt(1 - u) * torch.cos(2 * math.pi * v),
+            torch.sqrt(u) * torch.sin(2 * math.pi * w),
+            torch.sqrt(u) * torch.cos(2 * math.pi * w),
+        ],
+        dim=-1,
+    )
+
+class GaussianSSC(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.init_num_points = kwargs.get("num_points", 2000)
+        self.H, self.W, self.L = kwargs["H"], kwargs["W"], kwargs["L"]  # Note, here H, W, L are the dimension of x, y, z
+        self.BLOCK_W, self.BLOCK_H, self.BLOCK_L = kwargs["BLOCK_W"], kwargs["BLOCK_H"], kwargs["BLOCK_L"]
+        self.tile_bounds = (
+            (self.W + self.BLOCK_W - 1) // self.BLOCK_W,
+            (self.H + self.BLOCK_H - 1) // self.BLOCK_H,
+            (self.L + self.BLOCK_L - 1) // self.BLOCK_L,
+        )
+        
+        self.lidar_mins = kwargs.get("lidar_mins", [-10, -10, -10])
+        
+        self._xyz = nn.Parameter(torch.atanh(2 * (torch.rand(self.init_num_points, 3) - 0.5)))  # 通过与后面的thanhh函数，得到-1到1之间的数. 做了范围限制
+
+        self._scaling = nn.Parameter(torch.log(torch.rand(self.init_num_points, 3)))
+
+        # self.register_buffer('_opacity', torch.ones((self.init_num_points, 1)))
+        # self._opacity = nn.Parameter(torch.logit(0.5 * torch.ones(self.init_num_points, 1))) # 限制在0-1之间
+        # self._opacity = nn.Parameter(10 * torch.rand(self.init_num_points, 1))  # 结合 exp 保证 > 0
+        self._opacity = nn.Parameter(torch.rand(self.init_num_points, 1))
+        self._rotation = nn.Parameter(random_quat_tensor(self.init_num_points))
+
+        # self._features_dc = nn.Parameter(torch.rand(self.init_num_points, 2)) 
+        # self.register_buffer('_features_dc', torch.ones((self.init_num_points, 1)))
+
+    @property
+    def get_xyz(self):
+        return torch.tanh(self._xyz) 
+
+    @property
+    def get_scaling(self):
+        return torch.exp(self._scaling)
+        # 返回 torch.exp(self._scaling) 的结果
+    @property
+    def get_opacity(self):
+        # return torch.exp(self._opacity) # 保证 > 0
+        return self._opacity
+    
+    @property
+    def get_rotation(self):
+        return torch.nn.functional.normalize(self._rotation)  # 旋转四元数模长为1
+    
+    def forward(self, x):
+        # rendering: x: B, N, 3 要渲染的位置
+        
+        xys, depths, radii, conics, num_tiles_hit = project_gaussians(self.get_xyz, self.get_scaling, 1, 
+                                                                                       self.get_rotation, self.H, self.W, self.L,
+                                                                                            self.tile_bounds)
+        return rasterize_gaussians_sum(x, xys, depths, radii, conics, num_tiles_hit, 
+                                        # self._features_dc, 
+                                       self.get_opacity, 
+                                       self.H, self.W, self.L,
+                                       self.BLOCK_W, self.BLOCK_H, self.BLOCK_L,
+                                       self.lidar_mins 
+                                       )
+
+
+if __name__ == '__main__':
+    
+    # ------------------------------------------------------------------------------------------------------------------
+    # 测试cube拟合
+    cube_x = 100
+    cube_y = 100
+    cube_z = 100
+    
+    gt_image = torch.zeros((cube_x, cube_y, cube_z))
+    # make top left and bottom right red, blue
+    gt_image[: cube_x // 2, : cube_y // 2, cube_z // 2 :] = torch.tensor([1.0])
+    gt_image[cube_x // 2 :, cube_y // 2 :, :cube_z // 2] = torch.tensor([1.0])
+        
+    # normlize
+    mins = [0., 0., 0.]
+    
+    grid_size = 6
+    
+    num_channel = 2  # 占据或者不占据
+    num_points = 100 # 高斯点
+    
+    # 我这样做其实是一个生成式的3D模型，雷达是采样的点
+    gaussian_model = GaussianSSC(num_points=num_points, H = cube_x, W = cube_y, L = cube_z, BLOCK_W = grid_size, BLOCK_H = grid_size, BLOCK_L = grid_size, lidar_mins=mins).cuda()
+    
+    steps = 500
+
+    # loss_fn = nn.CrossEntropyLoss(weight=torch.tensor([0.8, 1]).cuda())
+    loss_fn = nn.BCEWithLogitsLoss(weight=torch.tensor([1.])).cuda()
+    # loss_fn = nn.BCELoss(weight=torch.tensor([1.])).cuda()
+
+    optimizer = torch.optim.Adam(gaussian_model.parameters(), lr=0.01)
+    # scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=500, gamma=0.5)
+    gaussian_model.train()
+
+    # --------------------------------------------------------------
+    start = 0
+    end = 100
+    step = 1
+    x = np.arange(start, end, step)
+    y = np.arange(start, end, step)
+    z = np.arange(start, end, step)
+
+    # Generate all combinations of coordinates
+    X, Y, Z = np.meshgrid(x, y, z, indexing='ij')
+    points = np.stack((X, Y, Z), axis=-1).reshape(-1, 3)
+    target = gt_image[points[:, 0], points[:, 1], points[:, 2]]
+    # --------------------------------------------------------------
+
+    progress_bar = tqdm(range(steps), desc="Training")
+    
+    vis_data = []
+    for i in progress_bar:
+    
+        sampled_points = torch.from_numpy(points).cuda().float()
+        sampled_target = target.cuda().float()
+
+        out = gaussian_model.forward(sampled_points) # N * 1; 渲染就是全 0是初始化的问题
+        # out = out.squeeze(1)
+        loss = loss_fn(out, sampled_target)
+        # torch.nn.utils.clip_grad_norm_(gaussian_model.parameters(), max_norm=1.0)
+        loss.backward()   
+        optimizer.step()
+        optimizer.zero_grad()
+        
+        progress_bar.set_description(f"Step {i}, Loss: {loss.item()}")
+        if i % 20 == 0:
+            vis_data.append(
+                {
+                    "out": out.sigmoid().cpu().detach().numpy(),     
+                    "xyz": gaussian_model.get_xyz.cpu().detach().numpy(),
+                    "scale": gaussian_model.get_scaling.cpu().detach().numpy(),
+                    "quat": gaussian_model.get_rotation.cpu().detach().numpy(),
+                    "opacity": gaussian_model.get_opacity.cpu().detach().numpy(),
+                }       
+                # out.sigmoid().cpu().detach().numpy() 
+            )
+
+
+
+
+import pickle
+
+with open("./samples/vis_data.pkl", "wb") as f:
+    pickle.dump(vis_data, f)
